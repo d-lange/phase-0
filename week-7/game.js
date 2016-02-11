14f79c812d33868ce68891f31cd8ea1bf4431e3a
d-lange <dlange8181@gmail.com>
2016-02-11 02:05:07 -0500

    create game.js file

---
+++ week-7/game.js
@@ -0,0 +1,175 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+
+confirm("Are you ready to play my game?");
+
+var user = {
+  name: prompt("What is your name?"),
+  age: prompt("How old are you?")
+}
+
+var computer = {
+
+}
+
+// var userGuess = prompt("Try and guess the number I'm thinking of from 0 to 10.");
+
+// var computerChoice = Math.floor(Math.random()*10);
+
+// var guess = function (num) {
+//     if (userGuess === computerChoice) {
+//       console.log("Wow, great job! You guessed it!");
+//     }
+//     else if (userGuess < computerChoice) {
+//       console.log("A little low, try again.");
+//     }
+//     else if (userGuess > computerChoice) {
+//       console.log("A little high, try again.");
+//     }
+//     else {
+//       console.log("Looks like you didn't get it. The right number was " + computerChoice + ".");
+//     }
+// };
+
+// guess(8);
+  // else if (guess2 === computerChoice) {
+  //   console.log("Nice! You guessed it on the second try!");
+  // }
+  // else if (guess3 === computerChoice) {
+  //   console.log("Nice! You guessed it on the third try!");
+  // }
+  // else if (guess4 === computerChoice) {
+  //   console.log("Nice! You guessed it on the fourth try!");
+  // }
+  // else if (guess5 === computerChoice) {
+  //   console.log("You guessed it on your last try!");
+  // }
+  // else {
+  //   console.log("Looks like I win since you didn't guess the number I was thinking of!");
+  // }
+//};
+
+
+//Adventure game
+/*confirm("I am ready to play!");
+
+var age = prompt("What's your age");
+
+if(age < 13)
+{
+    console.log("You're allowed to play but I take no responsibility!");
+}
+else
+{
+    console.log("Awesome! Enjoy playing!");
+}
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+userAnswer = prompt("Do you want to race Bieber on stage?");
+
+if (userAnswer === "yes")
+{
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+
+feedback = prompt("Please rate my game out of 10");
+
+if (feedback > 8)
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+else
+{
+    console.log("I'll keep practicing coding and racing.");
+}*/
+
+// Rock, paper, scissors game
+
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+  computerChoice = "paper";
+} else {
+  computerChoice = "scissors";
+} console.log("Computer chose: " + computerChoice);
+
+var compare = function (choice1, choice2) {
+    if (choice1 === choice2) {
+        return "The result is a tie!";
+    }
+
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "You win because you chose rock and rock beats scissors";
+        }
+        else {
+            return "Looks like the computer chose paper, which wins and beats rock";
+        }
+    }
+
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "You win because you chose paper and paper beats rock";
+        }
+        else {
+            return "Looks like the computer chose scissors, which wins and beats paper";
+        }
+    }
+
+    else if (choice1 === "scissors") {
+        if (choice2 === "paper") {
+            return "You win because you chose scissors and scissors beats paper";
+        }
+        else {
+            return "Looks like the computer chose rock, which wins and beats scissors";
+        }
+    }
+};
+
+compare(userChoice, computerChoice);*/
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
